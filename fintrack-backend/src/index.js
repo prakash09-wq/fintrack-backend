b134af85c131120b217858dbcbd898cef8d28659
Jay Prakash <prakashaatish@gmail.com>
2026-06-07 23:03:24 +0530

    Update index.js

--- fintrack-backend/src/index.js
+++ fintrack-backend/src/index.js
@@ -96,7 +96,7 @@ async function startServer() {
   try {
     await prisma.$connect();
     console.log("✅ PostgreSQL connected via Prisma");
-    app.listen(PORT, () => {
+    app.listen(PORT,'0.0.0.0' () => {
       console.log(`🚀 FinTrack API running on http://localhost:${PORT}`);
       console.log(`📋 Environment: ${process.env.NODE_ENV || "development"}`);
     });
